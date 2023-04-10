8396964bb156c3da3d2a798424782089f0d8356f
Gad Diego Andres Sanchez Ortega <diego.sanchez@concentrix.com>
2023-04-09 19:10:19 -0700

    479- replacing Post for PostList middle component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Post from './components/Post';
+import PostList from './components/PostList';
 
 function App() {
   return (
     <>
-      <Post author="test1" body="test msj"/>
-      <Post author="test2" body="test msj2"/>
-      <Post author="test3" body="test msj3"/>
-      <Post author="test4" body="test msj4"/>
+      <PostList/>
     </>
   );
 }
---
+++ src/components/PostList.jsx
@@ -0,0 +1,16 @@
+
+import Post from "./Post";
+
+function PostList(props){
+    return (
+        <>  
+            <Post author="test1" body="test msj"/>
+            <Post author="test2" body="test msj2"/>
+            <Post author="test3" body="test msj3"/>
+            <Post author="test4" body="test msj4"/>
+        </>
+    );
+
+}
+
+export default PostList;
