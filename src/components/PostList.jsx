a7b0207415a25afc206230bbaf811e0220ea5530
Gad Diego Andres Sanchez Ortega <diego.sanchez@concentrix.com>
2023-04-09 20:40:51 -0700

    483, Lifting state up

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,21 +1,35 @@
+import { useState } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
 
-function PostList(props){
-    return (
-        <>  
-        <NewPost/>
-        <ul>
-            <Post author="test1" body="test msj"/>
-            <Post author="test2" body="test msj2"/>
-            <Post author="test3" body="test msj3"/>
-            <Post author="test4" body="test msj4"/>
-        </ul>
-            
-        </>
-    );
+function PostList(props) {
+  const [enteredBody, setEnteredBody] = useState(""); //Only useState[0] & useState[1] are valid, nothing more, nothing less.
+  const [enteredAuthor, setEnteredAuthor] = useState(""); //Only useState[0] & useState[1] are valid, nothing more, nothing less.
 
+  function bodyChangeHandler(event) {
+    event.preventDefault();
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    event.preventDefault();
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
+      <ul>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="test2" body="test msj2" />
+        <Post author="test3" body="test msj3" />
+        <Post author="test4" body="test msj4" />
+      </ul>
+    </>
+  );
 }
 
 export default PostList;
