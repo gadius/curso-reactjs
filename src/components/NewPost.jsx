aa067a70a01fdcc122d734e9de28fa71d4303850
Gad Diego Andres Sanchez Ortega <diego.sanchez@concentrix.com>
2023-04-09 19:42:31 -0700

    482- useState hook on NewPost example

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,28 @@
+import classes from './NewPost.module.css';
+import { useState } from 'react';
+
+function NewPost() {
+
+    const [enteredBody, setEnteredBody] = useState(''); //Only useState[0] & useState[1] are valid, nothing more, nothing less.
+
+    function changeBodyHandler(event) {
+        event.preventDefault();
+        setEnteredBody(event.target.value);
+    }
+
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+      </p>
+      <p>{enteredBody}</p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required />
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,18 @@
 
 import Post from "./Post";
+import NewPost from "./NewPost";
 
 function PostList(props){
     return (
         <>  
+        <NewPost/>
+        <ul>
             <Post author="test1" body="test msj"/>
             <Post author="test2" body="test msj2"/>
             <Post author="test3" body="test msj3"/>
             <Post author="test4" body="test msj4"/>
+        </ul>
+            
         </>
     );
 
